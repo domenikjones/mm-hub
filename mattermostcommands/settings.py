345fc6d3b110e7c058e9ecfb4fe05e2c686c0dbb
Domenik Jones <d.jones@s-v.de>
2016-11-17 16:42:42 +0100

    init

--- mattermostcommands/settings.py
+++ mattermostcommands/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$+o4q8#$msrujooa&=1rt(6&g1jg3+si2rb!$7c^qbnof6n*_i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['138.68.89.118', ]
 
 
 # Application definition
