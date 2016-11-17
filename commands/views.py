bf87d771a1ce638c2b81c00e1ff910c5d4a53734
Domenik Jones <d.jones@s-v.de>
2016-11-17 16:47:37 +0100

    testing

--- commands/views.py
+++ commands/views.py
@@ -5,8 +5,14 @@ from django.http.response import HttpResponse
 
 def giphy_remote(request):
 
+    print("GET: %s" % request.GET)
+    print("POST: %s" % request.POST)
+
     json_data = {
         'text': "this is a test commands response",
     }
 
-    return HttpResponse(json.dumps(json_data))
+    json_dump = json.dumps(json_data)
+    print(json_dump)
+
+    return HttpResponse(json_dump)
