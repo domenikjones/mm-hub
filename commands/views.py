93be6c5764bac700b99cb2230cb6d4f173471714
Domenik Jones <d.jones@s-v.de>
2016-11-17 17:37:59 +0100

    testing

--- commands/views.py
+++ commands/views.py
@@ -1,30 +1,45 @@
 import json
+import requests
 
 from django.http.response import HttpResponse
 
 
+def test_giphy(request):
+
+    body = "channel_id=5p9xd4brfincpgr5mjnpm31sko&channel_name=testing-commands&command=%2Fgiphy&response_url=not+supported+yet&team_domain=unit-d&team_id=ukfjbw1113yh8koubk7ocumfgw&text=sdfasdfsdf&token=1nd87ztd3ib5mckuekqxxtsjow&user_id=cgqfob68cjnm8bap3rwz8yhshw&user_name=domjones"
+    r = requests.get('http://localhost:8081/giphy', body=body, )
+
+    return HttpResponse(True)
+
+
 def giphy_remote(request):
-
-    print("Body: %s" % request.body)
-    print("GET: %s" % request.GET)
-    print("POST: %s" % request.POST)
-
-    #1nd87ztd3ib5mckuekqxxtsjow
-
+    gif = ""
     dict = {}
     args = request.body.split('&')
+
+    text = None
     for arg in args:
-        arg_tmp = arg.split('0')
+        arg_tmp = arg.split('=')
         dict[arg_tmp[0]] = arg_tmp[1]
 
-    print(dict)
+    keywords = dict.get('text', None)
+
+    if keywords:
+        gif = "(%s)" % query_giphy_api(keywords)
 
     json_data = {
         "response_type": "in_channel",
-        'text': "this is a test commands response",
+        'text': gif,
     }
-
     json_dump = json.dumps(json_data)
-    print(json_dump)
-
     return HttpResponse(json_dump)
+
+
+def query_giphy_api(keywords):
+    url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=american+psycho"
+    r = requests.get(url)
+    dict = json.loads(r.text)
+    data = dict.get('data', None)
+    if data:
+        return data.get('url', None)
+    return None
